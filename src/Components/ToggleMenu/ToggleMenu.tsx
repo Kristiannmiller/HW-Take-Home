a4e9325475d4fe39f97ac841a8c55f42136cfc5a
Kristi Miller <kristi.ann.miller@gmail.com>
2021-05-04 12:06:05 -0600

    Add styles to ToggleMenu component

--- src/Components/ToggleMenu/ToggleMenu.tsx
+++ src/Components/ToggleMenu/ToggleMenu.tsx
@@ -2,9 +2,12 @@ import './ToggleMenu.css';
 
 function ToggleMenu() {
   return (
-    <>
-    ToggleMenu
-    </>
+    <section className="toggle-menu">
+      <a className="toggle-option">Premium Range</a>
+      <a className="toggle-option">Markets</a>
+      <a className="toggle-option">Industries</a>
+      <a className="toggle-option">Products</a>
+    </section>
   );
 }
 
