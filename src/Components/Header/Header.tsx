f82b0b554330294bb9e6968e049101b215204adc
Kristi Miller <kristi.ann.miller@gmail.com>
2021-05-04 11:22:13 -0600

    Add styling to Header component

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,27 @@
 import './Header.css';
+import arrow from '../../Assets/BackArrow.png';
+import bell from '../../Assets/Bell.png';
+import user from '../../Assets/user.png';
 
 function Header() {
   return (
-    <>
-    Header
-    </>
+    <header>
+      <section className="header-nav">
+        <img className="arrow" src={arrow} title="navigate back" alt="navigate back to main page"/>
+        <section className="user-data">
+          <h5>Elwin Sharvin</h5>
+          <img className="user" src={user} title="user image" alt="user"/>
+          <img className="bell" src={bell} title="user settings" alt="adjust user settings"/>
+        </section>
+      </section>
+      <section className="broker-data">
+        <h2>Broker Name</h2>
+        <article className="broker-context">
+          <p><span className="data-title">Website</span>www.website.com</p>
+          <p><span className="data-title">Summary</span>Travelers is the second largest writer of U.S. commercial property casualty insurace and the third largest writer of U.S. personal insurance through independent agents.</p>
+        </article>
+      </section>
+    </header>
   );
 }
 
