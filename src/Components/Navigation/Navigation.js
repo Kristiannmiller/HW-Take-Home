8bcbb7581b0df64a49821baab9d74208dfc17b30
Kristi Miller <kristi.ann.miller@gmail.com>
2021-05-04 18:26:46 -0600

    Add active state to Navigation component

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import './Navigation.css';
 function Navigation() {
   return (
     <section className="navigation">
-      <a className="nav-btn">SNAPSHOT</a>
+      <a className="nav-btn active-nav">SNAPSHOT</a>
       <a className="nav-btn">TRENDS</a>
     </section>
   );
