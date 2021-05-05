f1bf35a84fa16a0a53c82392c339c1175a6503bf
Kristi Miller <kristi.ann.miller@gmail.com>
2021-05-04 18:23:55 -0600

    Add active state to ToggleMenu component

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,36 @@
 import './Dashboard.css';
 import ToggleMenu from '../ToggleMenu/ToggleMenu'
 import BDCard from '../BDCard/BDCard'
+import { brokerQuery } from '../../apiCalls';
+import { useQuery } from '@apollo/client';
+import React, { useState, useEffect } from 'react';
+
 
 
 function Dashboard() {
+
+  const [premiumMenu, setPremiumMenu] = useState(false);
+  const [marketsMenu, setMarketsMenu] = useState(false);
+  const [industriesMenu, setIndustriesMenu] = useState(false);
+  const [productsMenu, setProductsMenu] = useState(false);
+
+  const adjustOptions = (optionToChange) => {
+    optionToChange === "premium" && setPremiumMenu(!premiumMenu)
+    optionToChange === "markets" && setMarketsMenu(!marketsMenu)
+    optionToChange === "industries" && setIndustriesMenu(!industriesMenu)
+    optionToChange === "products" && setProductsMenu(!productsMenu)
+  }
+
   return (
     <section className="dashboard">
-      <ToggleMenu />
+      <ToggleMenu
+        adjustOptions={adjustOptions}
+        selected={{
+          premium: premiumMenu,
+          markets: marketsMenu,
+          industries: industriesMenu,
+          products: productsMenu}}
+      />
       <section className="titles">
         <h3>Broker Book</h3>
         <h3>Carrier Placement</h3>
--- src/Components/ToggleMenu/ToggleMenu.js
+++ src/Components/ToggleMenu/ToggleMenu.js
@@ -1,12 +1,21 @@
 import './ToggleMenu.css';
 
-function ToggleMenu() {
+function ToggleMenu({ adjustOptions, selected }) {
+
+  const handleClick = (choice) => {
+    adjustOptions(choice)
+  }
+
   return (
     <section className="toggle-menu">
-      <a className="toggle-option">Premium Range</a>
-      <a className="toggle-option">Markets</a>
-      <a className="toggle-option">Industries</a>
-      <a className="toggle-option">Products</a>
+      <a onClick={() => handleClick("premium")}
+      className={selected.premium ? "toggle-option active" : "toggle-option"}>Premium Range</a>
+      <a onClick={() => handleClick("markets")}
+      className={selected.markets ? "toggle-option active" : "toggle-option"}>Markets</a>
+      <a onClick={() => handleClick("industries")}
+      className={selected.industries ? "toggle-option active" : "toggle-option"}>Industries</a>
+      <a onClick={() => handleClick("products")}
+      className={selected.products ? "toggle-option active" : "toggle-option"}>Products</a>
     </section>
   );
 }
